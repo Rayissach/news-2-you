ed533e171185244b54c1e709122fa8bf4c9dab6a
Raymond Robinson <rayissach@Raymonds-MacBook-Pro.local>
2018-03-19 22:56:57 -0700

    adding articles

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // var express = require("express");
 // var bodyParser = require("body-parser");
 // var logger = require("morgan");
-// var mongoose = require("mongoose");
+var mongoose = require("mongoose");
 // var request = require("request")
 // var cheerio = require("cheerio")
 // var axios = require("axios")
 // var mongojs = require('mongojs')
+
 
 var express = require("express");
 var mongojs = require("mongojs");
@@ -16,7 +17,7 @@ var cheerio = require("cheerio");
 // var PORT = 3000;
 
 // Require all models
-// var db = require("./models");
+var db = require("./models");
 
 // Initialize Express
 var app = express();
@@ -42,16 +43,17 @@ app.get("/", function(req, res) {
     res.json("Hello World")
 });
 
-app.get("/all",function(req, res) {
-    db.nytData.find({}, function(error, found) {
-        if(error) {
-            console.log(error)
-        }
-        else {
-            res.json(found)
-        }
-    })
-})
+// app.get("/all",function(req, res) {
+//     db.nytData.find({}, function(error, found) {
+//         if(error) {
+//             console.log(error)
+//         }
+//         else {
+//             res.json(found)
+
+//         }
+//     })
+// })
 
 app.get("/scrape", function(req, res) {
     // Make a request for the news section of ycombinator
@@ -79,17 +81,67 @@ app.get("/scrape", function(req, res) {
             else {
               // Otherwise, log the inserted data
               console.log(inserted);
+
+            //   res.json(inserted);
             }
           });
         }
       });
     });
-  
-    // Send a "Scrape Complete" message to the browser
-    res.send("Scrape Complete");
+});
+
+ // Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article
+      .find({})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
   });
-
-
+  
+  // Route for grabbing a specific Article by id, populate it with it's note
+  app.get("/articles/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article
+      .findOne({ _id: req.params.id })
+      // ..and populate all of the notes associated with it
+      .populate("note")
+      .then(function(dbArticle) {
+        // If we were able to successfully find an Article with the given id, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+  
+  // Route for saving/updating an Article's associated Note
+  app.post("/articles/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note
+      .create(req.body)
+      .then(function(dbNote) {
+        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
 
   app.listen(3000, function () {
     console.log("App running on port 3000 !");
